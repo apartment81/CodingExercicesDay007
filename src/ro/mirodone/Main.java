b08ec69621d024c7b2ae41c99c766b559e764eb9
apartment81 <bogdan.mirodone@gmail.com>
2019-05-09 20:06:57 +0300

    Input Calculator

--- src/ro/mirodone/Main.java
+++ src/ro/mirodone/Main.java
@@ -4,11 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        SpeedConverter speedConverter = new SpeedConverter();
+        //SpeedConverter testing code
+/*        SpeedConverter speedConverter = new SpeedConverter();
         double c = speedConverter.toMilesPerHour(4);
         System.out.println(c);
+        speedConverter.printConversion(-32.66);*/
 
-       speedConverter.printConversion(-32.66);
+        //InputCalculator testing code
+        InputCalculator calc = new InputCalculator();
+        calc.inputThenPrintSumAndAverage();
 
     }
 }
